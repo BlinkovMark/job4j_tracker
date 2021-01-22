c21d46dc77c29b66098e0ce195ec3f6bf8281dd4
BlinkovMark <markblinkov1@gmail.com>
2021-01-22 23:26:21 +0200

    3.1. Массив с пустыми ячейками [#400943]

---
+++ src/test/java/ru/job4j/pojo/ShopTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.pojo;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
+public class ShopTest {
+    @Test
+    public void whenLastNull() {
+        Product products[] = new Product[5];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        products[2] = new Product("Egg", 19);
+        int rsl = Shop.indexOfNull(products);
+        assertThat(rsl, is(3));
+    }
+
+    @Test
+    public void whenFirstNull() {
+        Product products[] = new Product[5];
+        products[1] = new Product("Milk", 10);
+        int rsl = Shop.indexOfNull(products);
+        assertThat(rsl, is(0));
+    }
+
+    @Test
+    public void whenHasNotNull() {
+        Product products[] = new Product[1];
+        products[0] = new Product("Milk", 10);
+        int rsl = Shop.indexOfNull(products);
+        assertThat(rsl, is(-1));
+    }
+
+    @Test
+    public void when4HasNull() {
+        Product products[] = new Product[5];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 5);
+        products[2] = new Product("Egg", 16);
+        products[3] = new Product("Oil", 3);
+        int rsl = Shop.indexOfNull(products);
+        assertThat(rsl, is(4));
+    }
+
+}
