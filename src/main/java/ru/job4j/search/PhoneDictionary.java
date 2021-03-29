d7d39780dba08c630c0d49a067aebbf806fbfac9
BlinkovMark <markblinkov1@gmail.com>
2021-03-30 01:11:44 +0300

    1. Телефонный справочник на базе ArrayList [#400848]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,7 +1,6 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class PhoneDictionary {
     private ArrayList<Person> persons = new ArrayList<Person>();
@@ -10,15 +9,18 @@ public class PhoneDictionary {
         this.persons.add(person);
     }
 
-    public ArrayList<Person> find(String key) {
-        ArrayList<Person> result = new ArrayList<>();
-        for (Person rsl : persons)
-        if (rsl.getName().contains(key) ||
-                rsl.getSurname().contains(key) ||
-                rsl.getAddress().contains(key) ||
-                rsl.getPhone().contains(key)) {
-            result.add(rsl);
-        }
+    public ArrayList<Person> find(String key) throws Error404Exception {
+            ArrayList<Person> result = new ArrayList<>();
+            for (Person rsl : persons) {
+                if (rsl.getName().contains(key) ||
+                        rsl.getSurname().contains(key) ||
+                        rsl.getAddress().contains(key) ||
+                        rsl.getPhone().contains(key)) {
+                    result.add(rsl);
+                } else {
+                    throw new Error404Exception("Error 404");
+                }
+            }
         return result;
     }
 }
--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -4,16 +4,24 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 public class PhoneDictionaryTest {
     @Test
-    public void whenFindByName() {
+    public void whenFindByName() throws Error404Exception {
         PhoneDictionary phones = new PhoneDictionary();
         phones.add(
-                new Person("Petr", "Arsentev", "534872", "Bryansk")
-        );
-        ArrayList<Person> persons = phones.find("Petr");
-        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+                new Person("Mark", "Blinkov", "55921184", "Tallinn"));
+        ArrayList<Person> persons = phones.find("Mark");
+        assertThat(persons.get(0).getSurname(), is("Blinkov"));
+    }
+
+
+    @Test(expected = Error404Exception.class)
+    public void whenNotFound() throws Error404Exception {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Mark", "Blinkov", "55921184", "Tallinn"));
+        phones.find("0");
     }
 }
