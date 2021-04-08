79dda0b7fd9fc5fc944c1cf54b0bf8f811e984f0
BlinkovMark <markblinkov1@gmail.com>
2021-04-08 21:09:03 +0300

    0. Сортировка [#400846]

--- src/test/java/ru/job4j/tracker/ItemTest.java
+++ src/test/java/ru/job4j/tracker/ItemTest.java
@@ -1,10 +1,9 @@
-package ru.job4j.tracker;
+package ru.job4j.tracker; //again
 
 import org.junit.Test;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
