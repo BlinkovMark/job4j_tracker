ff8297029ab3c5326003a8bda8949b22e623f704
BlinkovMark <markblinkov1@gmail.com>
2021-04-09 00:54:33 +0300

    2. Комбинированный компаратор [#400845]

---
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -0,0 +1,63 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+import java.util.Comparator;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
+public class JobTest {
+
+    @Test
+    public void whenComparatorByName() {
+        Comparator<Job> cmp = new ByNameSort();
+        int rsl = cmp.compare(
+                new Job("first", 0),
+                new Job("second", 1));
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenComparatorByNameReverse() {
+        Comparator<Job> cmp = new ByNameSortReverse();
+        int rsl = cmp.compare(
+                new Job("first", 0),
+                new Job("second", 1));
+        assertThat(rsl, greaterThan(0));
+    }
+
+    @Test
+    public void whenComparatorByPriority() {
+        Comparator<Job> cmp = new ByPrioritySort();
+        int rsl = cmp.compare(
+                new Job("first", 0),
+                new Job("second", 1));
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenComparatorByPriorityReverse() {
+        Comparator<Job> cmp =new ByPrioritySortReverse();
+        int rsl = cmp.compare(
+                new Job("first", 0),
+                new Job("second", 1));
+        assertThat(rsl, greaterThan(0));
+    }
+
+    @Test
+    public void whenComparatorByNameAndPriority() {
+        Comparator<Job> cmp = new ByNameSort().thenComparing(new ByPrioritySort());
+        int rsl = cmp.compare(
+                new Job("first", 0),
+                new Job("second", 1));
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenComparatorByNameAndPriorityReverse() {
+        Comparator<Job> cmp = new ByNameSortReverse().thenComparing(new ByPrioritySortReverse());
+        int rsl = cmp.compare(
+                new Job("first", 0),
+                new Job("second", 1));
+        assertThat(rsl, greaterThan(0));
+    }
+}
