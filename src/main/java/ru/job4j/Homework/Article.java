852e167517fe64c1ca84a8f0ffc0780527bfada8
BlinkovMark <markblinkov1@gmail.com>
2021-05-24 00:59:11 +0300

    1. Экзамен. Коллекции. [#10097 #158409]

--- src/main/java/ru/job4j/Homework/Article.java
+++ src/main/java/ru/job4j/Homework/Article.java
@@ -1,17 +1,19 @@
 package ru.job4j.Homework;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class Article {
     public static boolean generateBy(String origin, String line) {
-        boolean rsl = false;
-        String[] originText = origin.split("[ ,.!:;]");
-        String[] lineText = line.split(" ");
-        ArrayList<String> listOne = new ArrayList<>(Arrays.asList(originText));
-        ArrayList<String> listTwo = new ArrayList<>(Arrays.asList(lineText));
-        if (listOne.containsAll(listTwo)) {
-            rsl = true;
+        boolean rsl = true;
+        String[] originText = origin.replaceAll("\\p{P}", "").split(" ");
+        String[] lineText = line.replaceAll("\\p{P}", "").split(" ");
+        HashSet<String> listOne = new HashSet<>(Arrays.asList(originText));
+        for (String listTwo : lineText) {
+            if (!listOne.contains(listTwo)) {
+                rsl = false;
+                break;
+            }
         }
         return rsl;
     }
